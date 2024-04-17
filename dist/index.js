0210f350b43601335420e4f831e027229fff90d4
Vadim Kovalev <nikas.367@mail.ru>
2024-04-17 23:54:25 +0300

    код ревью

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,47 @@
 "use strict";
 const credit = document.querySelector('#credit');
-const rangeCredit = document.querySelector('#range-credit');
+const rangeCredit = document.querySelector('#rangeCredit');
 const month = document.querySelector('#month');
-const rangeMonth = document.querySelector('#range-month');
+const rangeMonth = document.querySelector('#rangeMonth');
 const monthlyInput = document.querySelector('#monthlyPayment');
-const AllSummaInput = document.querySelector('#AllSumma');
+const AllSummaInput = document.querySelector('#allSumma');
 //Сумма кредита
-rangeCredit === null || rangeCredit === void 0 ? void 0 : rangeCredit.addEventListener('mousedown', function () {
-    let intevalCredit = setInterval(() => {
-        credit.value = rangeCredit.value;
-    }, 100);
-    rangeCredit === null || rangeCredit === void 0 ? void 0 : rangeCredit.addEventListener('mouseup', function () {
-        clearInterval(intevalCredit);
+function rangeCreditValue() {
+    rangeCredit.addEventListener('mousedown', function () {
+        let intevalCredit = setInterval(() => {
+            credit.value = rangeCredit.value;
+        }, 0);
+        rangeCredit === null || rangeCredit === void 0 ? void 0 : rangeCredit.addEventListener('mouseup', function () {
+            clearInterval(intevalCredit);
+        });
     });
-});
+}
+!rangeCredit ? 0 : rangeCreditValue();
 //Срок кредита
-rangeMonth === null || rangeMonth === void 0 ? void 0 : rangeMonth.addEventListener('mousedown', function () {
-    let intevalMonth = setInterval(() => {
-        month.value = rangeMonth.value;
-    }, 100);
-    rangeMonth === null || rangeMonth === void 0 ? void 0 : rangeMonth.addEventListener('mouseup', function () {
-        clearInterval(intevalMonth);
+function rangeMonthValue() {
+    rangeMonth.addEventListener('mousedown', function () {
+        let intevalMonth = setInterval(() => {
+            month.value = rangeMonth.value;
+        }, 0);
+        rangeMonth === null || rangeMonth === void 0 ? void 0 : rangeMonth.addEventListener('mouseup', function () {
+            clearInterval(intevalMonth);
+        });
     });
-});
+}
+!rangeMonth ? 0 : rangeMonthValue();
 function formUpdate() {
     return setInterval(() => {
         let formValue = Array.from(new FormData(document.querySelector('form')));
-        const precent = Number(+formValue[2][1] < 10 ? '0.0' + +formValue[2][1] : '0.' + +formValue[2][1]);
-        const monthlyPayment = Math.round((Number(formValue[0][1]) * precent / 12) / (1 - ((1 + precent / 12)) ** (-formValue[1][1])));
+        const precent = Number(+formValue[2][1] < 10
+            ? '0.0' + +formValue[2][1]
+            : '0.' + +formValue[2][1]);
+        const monthlyPayment = Math.round((Number(formValue[0][1]) * precent / 12)
+            / (1 - ((1 + precent / 12))
+                ** (-formValue[1][1])));
         const AllSumma = (monthlyPayment * Number(formValue[1][1])).toFixed(2);
         monthlyInput.value = String(monthlyPayment);
         AllSummaInput.value = AllSumma;
-    }, 100);
+    }, 0);
 }
+;
 let interval = formUpdate();
--- index.ts
+++ index.ts
@@ -1,43 +1,60 @@
-const credit = document.querySelector('#credit') as HTMLInputElement
-const rangeCredit = document.querySelector('#range-credit') as HTMLInputElement;
-const month = document.querySelector('#month') as HTMLInputElement
-const rangeMonth = document.querySelector('#range-month') as HTMLInputElement;
+const credit = document.querySelector('#credit') as HTMLInputElement;
+const rangeCredit = document.querySelector('#rangeCredit') as HTMLInputElement;
+const month = document.querySelector('#month') as HTMLInputElement;
+const rangeMonth = document.querySelector('#rangeMonth') as HTMLInputElement;
 const monthlyInput = document.querySelector('#monthlyPayment') as HTMLInputElement;
-const AllSummaInput = document.querySelector('#AllSumma') as HTMLInputElement;
+const AllSummaInput = document.querySelector('#allSumma') as HTMLInputElement;
 
 //Сумма кредита
-rangeCredit?.addEventListener('mousedown', function(): void {
-  let intevalCredit = setInterval(() => {
-    credit.value = rangeCredit.value
-  }, 100)
+function rangeCreditValue() {
+  rangeCredit.addEventListener('mousedown', function(): void {
+    let intevalCredit = setInterval(() => {
+      credit.value = rangeCredit.value;
+    }, 0);
 
-  rangeCredit?.addEventListener('mouseup', function(): void {
-    clearInterval(intevalCredit)
-  })
-})
+    rangeCredit?.addEventListener('mouseup', function(): void {
+      clearInterval(intevalCredit);
+    });
+  });
+
+}
+
+!rangeCredit ? 0 : rangeCreditValue()
 
 //Срок кредита
-rangeMonth?.addEventListener('mousedown', function(): void {
-  let intevalMonth = setInterval(() => {
-    month.value = rangeMonth.value
-  }, 100)
+function rangeMonthValue() {
+  rangeMonth.addEventListener('mousedown', function(): void {
+    let intevalMonth = setInterval(() => {
+      month.value = rangeMonth.value;
+    }, 0);
 
-  rangeMonth?.addEventListener('mouseup', function(): void {
-    clearInterval(intevalMonth)
-  })
-})
+    rangeMonth?.addEventListener('mouseup', function(): void {
+      clearInterval(intevalMonth);
+    });
+  });
+}
+
+!rangeMonth ? 0 : rangeMonthValue()
 
 function formUpdate() {
   return setInterval(() => {
-    let formValue = Array.from(new FormData(document.querySelector('form') as HTMLFormElement))
+    let formValue = Array.from(new FormData(document.querySelector('form') as HTMLFormElement));
 
-    const precent: number = Number(+formValue[2][1] < 10 ? '0.0' + +formValue[2][1] : '0.' + +formValue[2][1])
-    const monthlyPayment: number = Math.round((Number(formValue[0][1]) * precent / 12) / (1 - ((1 + precent / 12)) ** (-formValue[1][1])))
-    const AllSumma: string = (monthlyPayment * Number(formValue[1][1])).toFixed(2)
+    const precent: number = Number(
+      +formValue[2][1] < 10
+      ? '0.0' + +formValue[2][1]
+      : '0.' + +formValue[2][1]
+    );
+    const monthlyPayment: number = Math.round((Number(
+      formValue[0][1]) * precent / 12)
+       / (1 - ((1 + precent / 12))
+        ** (-formValue[1][1]))
+    );
+    const AllSumma: string = (monthlyPayment * Number(formValue[1][1])).toFixed(2);
 
     monthlyInput.value = String(monthlyPayment);
     AllSummaInput.value = AllSumma;
-  }, 100)
-}
+  }, 0);
+};
 
-let interval = formUpdate()
+let interval = formUpdate();
